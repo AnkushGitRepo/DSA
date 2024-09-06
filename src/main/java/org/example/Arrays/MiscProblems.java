b5beefe5199e826d472d30fe02477f89dafbaad9
Ankush Gupta <ankushgupta1806@gmail.com>
2024-09-06 23:59:21 +0530

    Learned Selection Sort

--- src/main/java/org/example/Arrays/MiscProblems.java
+++ src/main/java/org/example/Arrays/MiscProblems.java
@@ -1,8 +1,9 @@
+
 class MiscProblems{
     public static void main(String[] args) {
-        int arr[] = {24,34,65,23,86};
-        int [] reveredArr = reverseArray(arr);
-        print(reveredArr);
+        int arr[] = {13,46,24,52,20,9};
+        int [] sortedArray = selectionSort(arr);
+        print(sortedArray);
     }
 
     public static int[] reverseArray(int [] arr){
@@ -19,5 +20,20 @@ class MiscProblems{
             System.out.print(arr[i]+" ");
         }
         System.out.println();
+    }
+
+    public static int[] selectionSort(int [] arr){
+        for (int i = 0; i <= arr.length - 2; i++){
+            int min = i;
+            for(int j = i; j <= arr.length - 1; j++){
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
     }
 }
